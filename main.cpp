5919ddb16c370e14e8d60b4700d655b284267ce7
kylebrain <kbrainard@nevada.unr.edu>
2019-09-05 11:18:59 -0700

    Config file is loaded from a command line argument

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <exception>
 
 #include "ConfigFile.h"
 
-int main(void)
+int main(int argc, char * argv[])
 {
-    ConfigFile configFile("config.txt");
-    std::cout << configFile << std::endl;
+    if(argc < 2)
+    {
+        std::cerr << "Please enter the config file name!" << std::endl;
+        return -1;
+    }
+
+    try
+    {
+        ConfigFile configFile(argv[1]);
+        std::cout << configFile << std::endl;
+    } catch (const std::exception & e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << "Simulation failed" << std::endl;
+        return -1;
+    }
+
     return 0;
 }
